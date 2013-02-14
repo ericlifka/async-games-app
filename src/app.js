89d9ea070abb14dcb1ca0d72fe72770e194764b9
eric.lifka <eric.lifka@gmail.com>
2013-02-13 22:04:30 -0500

    basicauth

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 var express = require('express'),
 	app = express();
 
+var authenticator = express.basicAuth(function(user, pass, callback){
+	callback(null, user === 'username' && pass === 'password');
+});
+
 app.use(express.static(__dirname + "/public"));
+app.get('/locked', authenticator, function (req, res) {
+	res.send('hi from an authenticated page.');
+});
+
 
 app.listen(3000);
 console.log('listening on port 3000');
